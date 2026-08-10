60d494d117f05d3d42ce3091665795f45655e90c
vdkhanh220119971-cloud <vdkhanh.22011997.1@gmail.com>
2026-08-10 11:12:19 +0700

    Update fishhook.h

--- fishhook.h
+++ fishhook.h
@@ -8,18 +8,12 @@
 extern "C" {
 #endif
 
-/*
- * Cấu trúc mô tả một symbol cần rebind (hook)
- */
 struct rebinding {
   const char *name;
   void *replacement;
   void **replaced;
 };
 
-/*
- * Hàm rebind_symbols nhận vào mảng các struct rebinding
- */
 int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel);
 
 #ifdef __cplusplus
